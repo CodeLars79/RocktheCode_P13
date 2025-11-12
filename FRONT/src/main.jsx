bd67b49425c1d97208d8d7cdaca3ff8420a2647a
Lars Sorensen <161650519+CodeLars79@users.noreply.github.com>
2025-11-12 20:29:30 +0100

    Add files via upload

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -2,13 +2,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { AuthProvider } from './utils/authContext.jsx'
-import CustomCursor from './components/CustomCursor/CustomCursor.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AuthProvider>
-      <CustomCursor />
       <App />
     </AuthProvider>
   </BrowserRouter>
